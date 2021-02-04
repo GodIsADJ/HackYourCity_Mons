a30e8aab64405324de73c3c546e81119ffe8f383
GodIsADJ <axelle.paquet+cv@gmail.com>
2021-02-04 11:42:37 +0100

    Last version

--- utils/main.py
+++ utils/main.py
@@ -4,4 +4,4 @@ from notable_trees import get_notable_trees, get_unique_list
 if __name__ == "__main__":
     df_trees = get_notable_trees()
     tree_coord_list = get_unique_list(df_trees)
-    print(tree_coord_list)
+    # print(tree_coord_list)
