11a838855870d1b409c7f7db0787454b529a61e2
Axl <axelle.paquet+git@gmail.com>
2021-02-05 11:27:35 +0100

    Demo ok

--- routes.py
+++ routes.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import jsonify, render_template
 from utils.map import get_map
+from utils.other_way import demo_map1
+from utils.second_way import demo_map2
 
 
 import random
@@ -22,11 +24,22 @@ def home():
 
 @app.route("/route")
 def analyse():
+    # TODO : request var from GET/POST 
     start_location = (50.454096, 3.9418326)
     end_location = (50.4537353, -3.9451452)
 
     data = get_map(start_location, end_location)
 
+    return render_template("route.html", data=data)
+
+@app.route("/route1")
+def demo1():
+    data = demo_map1()
+    return render_template("route.html", data=data)
+
+@app.route("/route2")
+def demo2():
+    data = demo_map2()
     return render_template("route.html", data=data)
 
 
---
+++ utils/second_way.py
@@ -0,0 +1,113 @@
+import osmnx as ox
+import networkx as nx
+import pandas as pd
+import folium
+from folium.plugins import MarkerCluster
+
+
+def demo_map2():
+    ox.config(log_console=True, use_cache=True)
+
+    G_walk = ox.graph_from_place('Mons, Hainaut, Belgium',simplify=False, network_type='bike')
+
+    orig_node = ox.get_nearest_node(G_walk,
+                                    (50.4571964, 3.9321058))
+
+    dest_node = ox.get_nearest_node(G_walk,
+                                    (50.4528003, 3.9602154))
+
+    route = nx.shortest_path(G_walk,
+                            orig_node,
+                            dest_node, weight='lenght')
+
+    ox.folium.plot_route_folium
+
+    route_map = ox.plot_route_folium(G_walk, route)
+
+
+
+    # récupère les coordonnées de la liste arbre
+    df_trees = pd.read_csv("Stuff/test.csv")
+    df_trees = df_trees.dropna()
+    tree_coord_list = df_trees["coord"].unique()
+
+    for i, coord in enumerate(tree_coord_list, start=0) : 
+
+        coord = tree_coord_list[i]
+        coord = coord.replace("[", "")
+        coord = coord.replace("]", "")
+        l = coord.split(",")
+
+        latitude = l[0]
+        longitude = l[1]
+
+        latitude = float(l[0])
+        longitude = float(l[1])
+        icon_url = 'static/icones/tree_icone.png'
+        popup = popup = '<strong>' + "Tilleul (Tilia)/Tilleul de Hollande : la légende raconte que le bois de ses ancêtres aurait servi à fabriquer des lances pour combattre le doudou" + '</strong>'
+        icon = folium.features.CustomIcon(icon_url,icon_size=(28, 30))  # Creating a custom Icon
+        folium.Marker([latitude, longitude], icon=icon, popup = popup).add_to(route_map)
+
+
+    icon_path = 'static/icones/station.png'
+    icon = folium.features.CustomIcon(icon_path,icon_size=(28, 30))
+    folium.Marker([50.454096, 3.9418326], icon=icon).add_to(route_map)
+
+    popup = popup = '<strong>' + 'hello doudou' + '</strong>'
+    icon_path = 'static/icones/musee_doudou.png'
+    icon = folium.features.CustomIcon(icon_path,icon_size=(28, 30))
+    folium.Marker([50.455476, 3.9517038], icon=icon, popup=popup).add_to(route_map)
+
+    df = pd.read_csv('Stuff/arceaux-velos_publication.csv',sep=';')
+    parking_bike = df['Geo Point']
+
+    for i, coord in enumerate(parking_bike, start=0) : 
+
+        l = coord.split(",")
+
+        latitude = l[0]
+        longitude = l[1]
+
+        icon_url = 'static/icones/parking_bike.png'
+        icon = folium.features.CustomIcon(icon_url,icon_size=(28, 30))  # Creating a custom Icon
+        folium.Marker([latitude,longitude], icon=icon).add_to(route_map)
+
+    df = pd.read_csv('Stuff/monuments_mons_lat_lon.csv')
+    monuments = df['coordinate']
+    df = df.dropna()
+
+    for i, coord in enumerate(latitude, start=0) : 
+
+        latitude = df['latitude'][i]
+        longitude = df['longitude'][i]
+
+        icon_url = 'static/icones/monument.png'
+        icon = folium.features.CustomIcon(icon_url,icon_size=(28, 30))  # Creating a custom Icon
+        folium.Marker([latitude,longitude], icon=icon).add_to(route_map)
+
+    icon_path = 'static/icones/parking_car.png'
+    icon = folium.features.CustomIcon(icon_path,icon_size=(28, 30))
+    folium.Marker([50.4571964, 3.9321058], icon=icon).add_to(route_map)
+
+    icon_path = 'static/icones/museum.png'
+    icon = folium.features.CustomIcon(icon_path,icon_size=(28, 30))
+    folium.Marker([50.4528003, 3.9602154], icon=icon).add_to(route_map)
+
+    icon_path = 'static/icones/bike.png'
+    icon = folium.features.CustomIcon(icon_path,icon_size=(28, 30))
+    folium.Marker([50.45871144, 3.95662095], icon=icon).add_to(route_map)
+
+    icon_path = 'static/icones/bike.png'
+    icon = folium.features.CustomIcon(icon_path,icon_size=(28, 30))
+    folium.Marker([50.45505764, 3.95287386], icon=icon).add_to(route_map)
+
+    icon_path = 'static/icones/bike.png'
+    icon = folium.features.CustomIcon(icon_path,icon_size=(28, 30))
+    folium.Marker([50.45535941, 3.95968002], icon=icon).add_to(route_map)
+
+    icon_path = 'static/icones/bike.png'
+    icon = folium.features.CustomIcon(icon_path,icon_size=(50, 50))
+    folium.Marker([50.454753, 3.940031], icon=icon).add_to(route_map)
+
+
+    return route_map._repr_html_()
